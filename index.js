b1e648df44a4bf4f5d73cf03ee11c4a4a8abe0e0
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-05-12 15:52:06 -0700

    completed most of task 2

--- index.js
+++ index.js
@@ -79,9 +79,17 @@ function Person(name, age) {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
 
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fueled = function (gallons) {
+  return this.tank + gallons;
+};
 
 /*
   TASK 3
